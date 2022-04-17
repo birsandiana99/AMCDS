98b696ed31fc8c9e810929ccda78cb5794629976
Barcoaie <florinel.barcan@gmail.com>
2022-04-17 19:45:21 +0300

    created utils for msg

--- src/main/java/client/Infrastructure/MainSystem.java
+++ src/main/java/client/Infrastructure/MainSystem.java
@@ -1,5 +1,8 @@
 package client.Infrastructure;
 
+import client.Utilities.MessageUtils;
+import main.CommunicationProtocol.*;
+
 import java.io.IOException;
 
 public class MainSystem {
@@ -11,15 +14,70 @@ public class MainSystem {
         Proc proc1 = new Proc(5004, 1, 5001);
         System.out.println("Starting Proc no. 1");
 
+        MessageUtils.write(proc1.socketHub.getOutputStream(), Message.newBuilder()
+                .setType(Message.Type.NETWORK_MESSAGE)
+                .setNetworkMessage(
+                        NetworkMessage.newBuilder()
+                                .setSenderHost(proc1.addr)
+                                .setSenderListeningPort(proc1.port)
+                                .setMessage(Message.newBuilder()
+                                        .setSystemId("sys-1")
+                                        .setFromAbstractionId("app")
+                                        .setType(Message.Type.PROC_REGISTRATION)
+                                        .setProcRegistration(ProcRegistration.newBuilder()
+                                                .setOwner(proc1.owner)
+                                                .setIndex(proc1.index)
+                                                .build())
+                                        .build())
+                                .build())
+                        .build());
+
         proc1.socketHub.close();
 
         Proc proc2 = new Proc(5005, 2, 5002);
         System.out.println("Starting Proc no. 2");
 
+        MessageUtils.write(proc2.socketHub.getOutputStream(), Message.newBuilder()
+                .setType(Message.Type.NETWORK_MESSAGE)
+                .setNetworkMessage(
+                        NetworkMessage.newBuilder()
+                                .setSenderHost(proc2.addr)
+                                .setSenderListeningPort(proc2.port)
+                                .setMessage(Message.newBuilder()
+                                        .setSystemId("sys-1")
+                                        .setFromAbstractionId("app")
+                                        .setType(Message.Type.PROC_REGISTRATION)
+                                        .setProcRegistration(ProcRegistration.newBuilder()
+                                                .setOwner(proc2.owner)
+                                                .setIndex(proc2.index)
+                                                .build())
+                                        .build())
+                                .build())
+                .build());
+
         proc2.socketHub.close();
 
         Proc proc3 = new Proc(5006, 3, 5003);
         System.out.println("Starting Proc no. 3");
+
+
+        MessageUtils.write(proc3.socketHub.getOutputStream(), Message.newBuilder()
+                .setType(Message.Type.NETWORK_MESSAGE)
+                .setNetworkMessage(
+                        NetworkMessage.newBuilder()
+                                .setSenderHost(proc3.addr)
+                                .setSenderListeningPort(proc3.port)
+                                .setMessage(Message.newBuilder()
+                                        .setSystemId("sys-1")
+                                        .setFromAbstractionId("app")
+                                        .setType(Message.Type.PROC_REGISTRATION)
+                                        .setProcRegistration(ProcRegistration.newBuilder()
+                                                .setOwner(proc3.owner)
+                                                .setIndex(proc3.index)
+                                                .build())
+                                        .build())
+                                .build())
+                .build());
 
         proc3.socketHub.close();
     }
--- src/main/java/client/Utilities/MessageUtils.java
+++ src/main/java/client/Utilities/MessageUtils.java
@@ -1,4 +1,25 @@
 package client.Utilities;
 
+import com.google.common.primitives.Ints;
+import main.CommunicationProtocol.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigInteger;
+
 public class MessageUtils {
+    public static void write(OutputStream out, Message m) throws IOException {
+        byte[] bytes = m.toByteArray();
+        out.write(Ints.toByteArray(bytes.length), 0, Integer.SIZE/Byte.SIZE);
+        out.write(bytes, 0, bytes.length);
+    }
+
+    public static Message read(InputStream in) throws IOException {
+        byte[] lengthMessage = new byte[Integer.SIZE/Byte.SIZE];
+        in.read(lengthMessage, 0, Integer.SIZE/Byte.SIZE);
+        byte[] message = new byte[new BigInteger(lengthMessage).intValue()];
+        in.read(message, 0, new BigInteger(lengthMessage).intValue());
+        return Message.parseFrom(message);
+    }
 }
